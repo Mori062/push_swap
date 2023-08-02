ccef479006c1cc47b6f8f2430eecc15597eff440
shmorish <01morishi@gmail.com>
2023-08-03 03:43:59 +0900

    modi

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,27 +6,13 @@
 /*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 03:17:45 by shmorish          #+#    #+#             */
-/*   Updated: 2023/08/03 03:43:08 by shmorish         ###   ########.fr       */
+/*   Updated: 2023/08/03 03:43:46 by shmorish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int    sort(int *args, int num)
-{
-    if (num = 1)
-        return (0);
-    else if (num == 2)
-        return (sort2(args));
-    else if (num == 3)
-        return (sort3(args));
-    else if (num == 4)
-        return (sort4(args));
-    else if (num == 5)
-        return (sort5(args));
-    else
-        return (sort_many(args));
-}
+
 // 座標圧縮
 int compress_num(int *args)
 {
